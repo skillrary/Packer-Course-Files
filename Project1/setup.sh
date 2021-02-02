eabf559fcc0846663a5ddd20f02bba9ef6d5ecd8
SkillRary <46976867+skillrary@users.noreply.github.com>
2021-02-02 13:58:52 +0530

    Create setup.sh

---
+++ Project1/setup.sh
@@ -0,0 +1,9 @@
+sudo yum install httpd -y
+sudo service httpd start
+sudo chkconfig httpd on
+
+sudo touch /var/www/html/index.html
+
+sudo chmod 777 /var/www/html/index.html
+
+echo "<h1>This app is deployed by packer <h1>" > /var/www/html/index.html
